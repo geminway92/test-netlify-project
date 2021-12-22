4b33c98606d5ea16aad333d156d2874ae584cd76
Gema Martin <geminway@live.com>
2021-12-22 20:06:48 +0100

    convertir en numero

--- netlify/functions/array-numeros/array-numeros.js
+++ netlify/functions/array-numeros/array-numeros.js
@@ -4,18 +4,19 @@ function isMayor(n1,n2,n3,n4,n5,n6) {
 
 const handler = async (event) => {
   try {
+
     const totalNumber =[]
-    const num1 = totalNumber.push(event.queryStringParameters.num1)
-    const num2 = totalNumber.push(event.queryStringParameters.num2)
-    const num3 = totalNumber.push(event.queryStringParameters.num3)
-    const num4 = totalNumber.push(event.queryStringParameters.num4)
-    const num5 = totalNumber.push(event.queryStringParameters.num5)
-    const num6 = totalNumber.push(event.queryStringParameters.num6)
-    console.log(totalNumber)
+    const num1 = totalNumber.push(parseInt(event.queryStringParameters.num1))
+    const num2 = totalNumber.push(parseInt(event.queryStringParameters.num2))
+    const num3 = totalNumber.push(parseInt(event.queryStringParameters.num3))
+    const num4 = totalNumber.push(parseInt(event.queryStringParameters.num4))
+    const num5 = totalNumber.push(parseInt(event.queryStringParameters.num5))
+    const num6 = totalNumber.push(parseInt(event.queryStringParameters.num6))
+    // const arrayNumeros = totalNumber.split(", ")
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${totalNumber}` }),
+      body: JSON.stringify({ message: totalNumber }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
