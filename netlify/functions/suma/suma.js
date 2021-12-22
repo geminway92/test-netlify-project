cbcb676c71fa6390ebb929a712b5b3a09913e858
Gema Martin <geminway@live.com>
2021-12-22 19:07:03 +0100

    cambios

--- netlify/functions/suma/suma.js
+++ netlify/functions/suma/suma.js
@@ -2,7 +2,12 @@
 const handler = async (event) => {
   const a = event.queryStringParameters.num1
   const b = event.queryStringParameters.num2
+
+  let numero1 = parseInt(a)
+  let numero2 = parseInt(b)
+
   try {
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `El resultado de ${a} + ${b} es ${a + b}` }),
